65e401985d3428b5c12b92444038f2e072b722dc
ingfredy1 <ing.fredy.caballero@gmail.com>
2021-08-27 19:25:16 -0500

    prueba

---
+++ tuplas.py
@@ -0,0 +1,10 @@
+#las tuplas no se pueden modificar, usan parentesis
+##append permite añadir un item al final de la lista.
+#extend separa el string [:-1]
+Numeros3=[]
+Numeros1=[1, 2, 3]
+Numeros2=[4, 5, 6]
+print (Numeros1)
+Numeros1.extend(Numeros2)
+
+print (Numeros1)
